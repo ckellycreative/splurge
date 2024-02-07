7d01b2e9bd650e0ac970860a16070d250c1cffe7
Chris Kelly <chris@ckellydesign.com>
2024-02-07 16:10:40 -0500

    Tweak AccountsNav

--- src/_components/AccountsNav.jsx
+++ src/_components/AccountsNav.jsx
@@ -10,9 +10,9 @@ function AccountsNav(props) {
             totalCash += +category.bankBalance 
             return (    
                 <li key={category.id} className="nav-item">
-                    <a onClick={(cat) => props.handleClickAccountsNavItem(category)} className={`row nav-link ${props.activeCashTrackingAccount.id == category.id && 'active'}`}>
-                        <div className="col col-sm-9 account-balance-list-name">{category.category_title} </div>
-                        <div className="col col-sm-3 account-balance-list-amount">
+                    <a onClick={(cat) => props.handleClickAccountsNavItem(category)} className={`row align-items-center nav-link ${props.activeCashTrackingAccount.id == category.id && 'active'}`}>
+                        <div className="col account-balance-list-name">{category.category_title} </div>
+                        <div className="col account-balance-list-amount">
                             <NumericFormat value={category.bankBalance} displayType={'text'} thousandSeparator={true} prefix={''} />
                         </div>
                     </a>
@@ -27,9 +27,9 @@ function AccountsNav(props) {
         <div>
             <ul className="nav nav-pills flex-column AccountsNav">
                 <li className="nav-item">
-                    <a onClick={(cat) => props.handleClickAccountsNavItem('')} className={`row fw-bold nav-link ${!props.activeCashTrackingAccount  && 'active'}`}>
-                        <div className="col col-sm-9 account-balance-list-name">All Accounts</div>
-                        <div className="col col-sm-3 account-balance-list-amount">
+                    <a onClick={(cat) => props.handleClickAccountsNavItem('')} className={`row align-items-center fw-bold nav-link ${!props.activeCashTrackingAccount  && 'active'}`}>
+                        <div className="col account-balance-list-name">All Accounts</div>
+                        <div className="col account-balance-list-amount">
                             <NumericFormat value={totalCash.toFixed(2)} displayType={'text'} thousandSeparator={true} prefix={''} />
                         </div>
                         
